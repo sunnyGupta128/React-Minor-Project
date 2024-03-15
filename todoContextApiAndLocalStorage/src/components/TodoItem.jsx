75d0ad6487ff1a59cccdfe459e0aab261d599b39
sunnyGupta128 <sunnygupta6497@gmail.com>
2024-03-15 17:56:43 +0530

    incomplete

--- todoContextApiAndLocalStorage/src/components/TodoItem.jsx
+++ todoContextApiAndLocalStorage/src/components/TodoItem.jsx
@@ -6,12 +6,32 @@ function TodoItem({todo}) {
     const  [isEditable, setIsEditable]=useState(false);
     const [todoMsg, setTodoMsg]=useState(todo.todo)
 
+    const editTodo=()=>{
+        updateTodo(todo.id, {...todo, todo: todoMsg});
+        setIsEditable(false);
+
+    }  
+
+    const toggleCompleted=()=>{
+        toggleComplete(todo.id)
+    }
+
   return (
-    <div>
-        <input type="checkbox" />
-        <input type="text" />
-        <button></button>
-        <button></button>
+    <div className={`${todo.complete ? "bg-green-200 ": "bg-purple-300"}`}>
+        <input type="checkbox" checked={todo.complete} onChange={toggleCompleted} />
+        <input type="text" value={todoMsg} onChange={(e)=>setIsEditable(e.target.value)} readOnly={!isEditable} />
+        <button onClick={()=>{
+            if(todo.complete) return;
+
+            if(isEditable){
+                editTodo();
+            }else{
+                setIsEditable((prev)=>!prev);
+            }
+        }}
+        disabled={todo.complete}
+        >{isEditable ? "📁" : "✏️"}</button>
+        <button onClick={() => removeTodo(todo.id)}>❌</button>
     </div>
   )
 }
