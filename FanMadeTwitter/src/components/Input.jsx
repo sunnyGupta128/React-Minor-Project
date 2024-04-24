d3d7e6e34164a61d2ccdf6b6fc9a5277d46ff741
sunnyGupta128 <sunnygupta6497@gmail.com>
2024-04-24 10:02:08 +0530

    Add Button and Input components

---
+++ FanMadeTwitter/src/components/Input.jsx
@@ -0,0 +1,29 @@
+import React, {useId} from 'react'
+
+const Input = React.forwardRef(function Input({
+    label,
+    type='text',
+    placeholder,
+    className='',
+    ...props
+}, ref){
+    const id= useId()
+    return (
+        <div className='w-full'>
+            {label && <label
+            className='inline-block mb-1 pl-1'
+            htmlFor={id} >
+                {label}
+            </label>}
+            <input
+            type={type}
+            className={`px-2 py-3 bg-white text-black rounded-lg w-full border border-gray-50 focus:outline-none focus:border-gray-200
+            ${className}`}
+            ref={ref}
+            id={id}
+            {...props}
+            />
+        </div>
+    )
+})
+export default Input
