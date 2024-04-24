0c787cd3408f6352411f4cc22c2917109ec39172
sunnyGupta128 <sunnygupta6497@gmail.com>
2024-04-24 09:37:02 +0530

    Add Container and LogoutBtn components to index.js

--- FanMadeTwitter/src/components/index.js
+++ FanMadeTwitter/src/components/index.js
@@ -1,11 +1,15 @@
 import Headers from "./Headers/Headers";
 import Footers from "./Footers/Footers";
 import Logo from "./Logo";
+import Container from "./Container/Container";
+import LogoutBtn from "./Headers/LogoutBtn";
 
 
 export {
     Headers,
     Footers,
     Logo,
+    Container,
+    LogoutBtn
 
 }
