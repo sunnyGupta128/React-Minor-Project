4c035504e24b08d1299fdb3d5f985014d1604501
sunnyGupta128 <sunnygupta6497@gmail.com>
2024-04-27 13:12:38 +0530

    Fix import order and add console logs for debugging in App.jsx

--- FanMadeTwitter/src/App.jsx
+++ FanMadeTwitter/src/App.jsx
@@ -1,26 +1,30 @@
 import React, { useEffect } from 'react'
 import { useState } from 'react'
 import './App.css'
+import authService from './appwrite/auth'
 import { useDispatch } from 'react-redux'
 import { login, logout } from './store/authSlice'
-import authService from './appwrite/auth'
 import { Outlet } from 'react-router-dom'
 import Footers from './components/Footers/Footers'
 import Headers from './components/Headers/Headers'
 
 function App() {
-  
+  // console.log(import.meta.env.VITE_APPWRITE_PROJECT_ID);
   const [loading, setLoading]= useState(true)
   const  dispatch= useDispatch();
   useEffect(() => {
     authService.getCurrentUser()
+  
     .then((userData)=>{
+      console.log(userData);
       if (userData) {
         dispatch(login({userData}))
       }else{
         dispatch(logout())
       }
       
+    }).catch((error)=>{
+      console.log(error)
     }).finally(()=>setLoading(false))
   }, [])
   
@@ -29,10 +33,12 @@ function App() {
     <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
       <div className="w-full block">
         <Headers/>
-        <main>
+        <main className='pb-8'>
           Todo: <Outlet/> 
         </main>
-        <Footers/>
+        <Footers 
+        className='w-full sticky mt-10'
+        />
       </div>
     </div>
   ): null
