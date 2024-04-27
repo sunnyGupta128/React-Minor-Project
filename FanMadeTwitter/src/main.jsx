aa9d2fb5f4463a6d9537a818be895210496ac684
sunnyGupta128 <sunnygupta6497@gmail.com>
2024-04-27 13:12:56 +0530

    Add new routes and components for signup, login, and adding a post

--- FanMadeTwitter/src/main.jsx
+++ FanMadeTwitter/src/main.jsx
@@ -4,9 +4,15 @@ import App from './App.jsx'
 import './index.css'
 import {Provider} from "react-redux"
 import store from './store/store.js'
-import { createBrowserRouter } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import { AuthLayout, Login } from './components/index.js'
+import Signup from './pages/Signup.jsx'
+import AllPosts from './pages/AllPosts.jsx'
+import AddPost from './pages/AddPost.jsx'
+import EditPost from './pages/EditPost.jsx'
+import Post from './pages/Post.jsx'
+ 
 
 const router = createBrowserRouter([
   {
@@ -23,7 +29,7 @@ const router = createBrowserRouter([
           <AuthLayout authentication={false}>
             <Login />
           </AuthLayout>
-        )
+        ),
       },
       {
         path: "/signup",
@@ -64,15 +70,15 @@ const router = createBrowserRouter([
         path: "/post/:slug",
         element: <Post />,
     },
-    ]
+    ],
 
-  }
+  },
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  
     <Provider store={store}>
       <RouterProvider router={router}/>
     </Provider>
-  </React.StrictMode>,
+  
 )
